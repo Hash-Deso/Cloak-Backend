e344bae55b0df29aa21930c3990e89ff7cd42af2
Major2001 <sanchitgupta1230@gmail.com>
2022-01-03 17:56:42 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const axios = require('axios').default;
+require('dotenv').config({ path: './config.env' });
+const cors = require('cors');
+const tf = require('@tensorflow/tfjs-node');
+const nsfw = require('nsfwjs');
+const sharp = require('sharp');
+let model = null;
+
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.post('/detect', async (req, res, next) => {
+  try {
+    const { images } = req.body;
+    images.shift();
+    console.log(images);
+    if (!images) {
+      throw new Error('Bad Request');
+    }
+    const result = [];
+    if (!model) {
+      console.log('Loading model');
+      model = await nsfw.load();
+    }
+    for (let i = 0; i < images.length; i++) {
+      const pic = await axios.get(images[i], {
+        responseType: 'arraybuffer',
+      });
+      const jpegBuffer = await sharp(pic.data).jpeg().toBuffer();
+      const image = tf.node.decodeImage(jpegBuffer, 3);
+      const predictions = await model.classify(image);
+      const importantClasses = ['Hentai', 'Porn', 'Sexy'];
+      for (let j = 0; j < predictions.length; j++) {
+        const prediction = predictions[j];
+        if (
+          importantClasses.includes(prediction.className) &&
+          prediction.probability * 100 >= 30
+        ) {
+          result.push(images[i]);
+        }
+      }
+      image.dispose();
+    }
+    res.status(200).json({ status: 'success', result });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ status: 'error', error: err.message });
+});
+const PORT = process.env.PORT || 3000;
+nsfw.load().then((main) => {
+  model = main;
+  app.listen(PORT, () => {
+    console.log('Server Started');
+  });
+});
